7961a2677b9d5736cf11b885d38259903fe9b769
Diego Cerda <diego.cerda200218@gmail.com>
2024-12-11 14:28:29 -0300

    v1.4

---
+++ etprogra/js/JavaScript.js
@@ -0,0 +1,14 @@
+
+function UserAgent(){
+
+}
+
+function Alerta(){
+    Swal.fire({
+    icon: "success",
+    title: "¡El archivo ha sido descargado!",
+    showConfirmButton: false,
+    timer: 3000
+    });
+}
+
